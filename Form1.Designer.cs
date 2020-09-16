3eefc4abf15b0ce53726b9be807341a219ce565b
ЗиНЧуК МаКС <roma23melinick@Gmail.com>
2020-09-16 21:56:31 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Tovars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TovarNAme = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.TovarNAme,
+            this.Cost});
+            this.dataGridView1.Location = new System.Drawing.Point(52, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(495, 412);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // TovarNAme
+            // 
+            this.TovarNAme.HeaderText = "Name";
+            this.TovarNAme.Name = "TovarNAme";
+            this.TovarNAme.ReadOnly = true;
+            this.TovarNAme.Width = 200;
+            // 
+            // Cost
+            // 
+            this.Cost.HeaderText = "Cost";
+            this.Cost.Name = "Cost";
+            this.Cost.ReadOnly = true;
+            this.Cost.Width = 150;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(553, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Tovar";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(553, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.button1.Location = new System.Drawing.Point(552, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 70);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(553, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(552, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Цена";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.5F);
+            this.button2.Location = new System.Drawing.Point(552, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Коректировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(553, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(668, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 29);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Загрузить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Tovars";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TovarNAme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cost;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Tovars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string allowed = "qwertyuiopasdgfhjklzxcvbvnmQWERTYUIOPASDFGHJKLZXCVBBNMM";
+        int newIndexes = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Convert.ToInt32(textBox2.Text);
+             }
+            catch (Exception)
+            {
+                textBox2.Text = "0";
+                return;
+            }
+            foreach (char item in textBox1.Text)
+            {
+                if (!allowed.Contains(item))
+                {
+                    textBox1.Text = "Tovar";
+                    return;
+                }
+            }
+
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[newIndexes].Cells[0].Value = newIndexes;
+            dataGridView1.Rows[newIndexes].Cells[1].Value = textBox1.Text;
+            dataGridView1.Rows[newIndexes].Cells[2].Value = textBox2.Text;
+            newIndexes++;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selected == dataGridView1.Rows.Count - 1) return;
+            try
+            {
+                Convert.ToInt32(textBox2.Text);
+            }
+            catch (Exception)
+            {
+                textBox2.Text = "0";
+                return;
+            }
+            foreach (char item in textBox1.Text)
+            {
+                if (!allowed.Contains(item))
+                {
+                    textBox1.Text = "Tovar";
+                    return;
+                }
+            }
+            try
+            {
+                dataGridView1.Rows[selected].Cells[1].Value = textBox1.Text;
+                dataGridView1.Rows[selected].Cells[2].Value = textBox2.Text;
+            }
+            catch
+            {
+
+            }
+
+            // newIndexes++;
+        }
+
+        public void save(string path) {
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+                    {
+                        writer.WriteLine(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+                writer.Close();
+            }
+
+        }
+
+        public void load(string path)
+        {
+            dataGridView1.Rows.Clear();
+            newIndexes = 0;
+            using (StreamReader writer = new StreamReader(path))
+            {
+                int lenght = 0;
+                string line;
+                while ((line = writer.ReadLine()) != null)
+                {
+                    if(lenght % 3 == 0)
+                    {
+                        newIndexes++;
+                        dataGridView1.Rows.Add();
+                    }
+                    dataGridView1.Rows[lenght / 3].Cells[lenght % 3].Value = line;
+                    lenght++;
+                }
+                writer.Close();
+            }
+
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        int selected = 0;
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            selected = e.RowIndex;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            save("Save.DBS");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            load("Save.DBS");
+
+        }
+    }
+}
